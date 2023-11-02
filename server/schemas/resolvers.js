8fa955ecf2553d0414fd88d6dda345a70540bd69
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2023-11-02 12:12:45 -0500

    Added schema folder content

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,65 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { Profile } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    profiles: async () => {
+      return Profile.find();
+    },
+
+    profile: async (parent, { profileId }) => {
+      return Profile.findOne({ _id: profileId });
+    },
+  },
+
+    Mutation: {
+        addProfile: async (parent, { name, email, password }) => {
+            const profile = await Profile.create({ name, email, password });
+            const token = signToken(profile);
+
+            return { token, profile };
+        },
+        login: async (parent, { email, password }) => {
+            const profile = await Profile.findOne({ email });
+
+            if (!profile) {
+                throw new AuthenticationError('No profile with this email found!');
+            }
+
+            const correctPw = await profile.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect password!');
+            }
+
+            const token = signToken(profile);
+            return { token, profile };
+        },
+
+        addFavorite: async (parent, { profileId, favorite }) => {
+            return Profile.findOneAndUpdate(
+                { _id: profileId },
+                {
+                $addToSet: { favorites: favorite },
+                },
+                {
+                    new: true,
+                    runValidators: true,
+                }
+            );
+        },
+        removeProfile: async (parent, { profileId }) => {
+            return Profile.findOneAndDelete({ _id: profileId });
+        },
+        removeFavorite: async (parent, { profileId, favorite }) => {
+            return Profile.findOneAndUpdate(
+                { _id: profileId },
+                { $pull: { favorites: favorite } },
+                { new: true }
+            );
+        },
+    },
+};
+
+module.exports = resolvers;
