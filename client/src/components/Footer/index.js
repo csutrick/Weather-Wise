d8e32be28e4d35a6dea2c230c1ca4f905b90efdf
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2023-11-22 19:51:08 -0600

    Bug fixed navbar

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className='w-full flex flex-row justify-center items-center'>
-      <div className='bg-gray-600 w-[1250px] h-[300px] flex flex-row justify-center items-center rounded-2xl'>
+      <div className='bg-gray-600 w-[1250px] h-[300px] mx-12 flex flex-row justify-center items-center rounded-2xl'>
         <h1 className='text-4xl font-bold text-white'>Footer</h1>
       </div>
     </footer>
