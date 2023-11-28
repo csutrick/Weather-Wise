a7c680532ecf992bab45313a25ea21765a038ea7
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2023-11-28 12:32:55 -0600

    Added user favorite content styles and layout

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -9,6 +9,7 @@ import SearchResults from '../components/SearchResults';
 const Search = () => {
   const [city, setCity] = useState('');
   const [weatherData, setWeatherData] = useState(null);
+  const profileId = ''
 
   const handleSearch = async (searchCity) => {
     try {
@@ -22,7 +23,7 @@ const Search = () => {
   return (
       <main className='w-full h-fit flex justify-center mt-28 mb-16'>
         <div className='bg-gray-300 w-[1250px] mx-12 flex flex-col p-2 rounded-lg'>
-          <Favorites />
+          <Favorites setCity={setCity} handleSearch={handleSearch}/>
           <div className='flex flex-col md:flex-row mt-2 p-2 border-2 border-black rounded-lg'>
             <Sidebar setCity={setCity} handleSearch={handleSearch}/>
             <SearchResults weatherData={weatherData}/>
